b335ed388fbd95c01a5b9444a032bac1ed60f69e
N <ntsang3@myseneca.ca>
2025-06-21 08:11:31 -0400

    Added Files
    GroqGPT.py
    MangoDB.py

---
+++ MangoDB.py
@@ -0,0 +1,8 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["mydatabase"]
+
+user_data = {"username": "new_user", "email": "new_user@example.com", "password_hash": "hashed_password"}
+users_collection.insert_one(user_data)
